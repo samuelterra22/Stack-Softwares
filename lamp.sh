2821e10bbcb421478216228d7dc70fe876a21add
Samuel Terra <samuelterra22@hotmail.com>
2017-09-14 10:12:04 -0300

    Script para instalação do Spotify adicionado.

--- lamp.sh
+++ lamp.sh
@@ -9,7 +9,7 @@ echo "########################################################"
 wget -bc "https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.9-1ubuntu16.04-amd64.deb"
 
 echo "########################################################"
-echo "               Instalndo Serviços LAMP                  "
+echo "               Instalando Serviços LAMP                  "
 echo "########################################################"
 
 # Update the repository
@@ -65,7 +65,7 @@ echo "########################################################"
 sudo apt install htop
 
 echo "########################################################"
-echo "                Instalndo o SpeedTest                   "
+echo "                Instalando o SpeedTest                   "
 echo "########################################################"
 
 wget -O speedtest "https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py"
@@ -73,12 +73,21 @@ sudo chmod +x speedtest
 sudo mv speedtest /usr/bin/speedtest
 
 echo "########################################################"
-echo "                   Instalndo Git                        "
+echo "                   Instalando Git                        "
 echo "########################################################"
 
 sudo apt-add-repository ppa:git-core/ppa -y
 sudo apt-get update
 sudo apt-get install git
+
+echo "########################################################"
+echo "                   Instalando Spotify                   "
+echo "########################################################"
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
 
 echo "########################################################"
 echo "               Instalando o Workbeanch                  "
