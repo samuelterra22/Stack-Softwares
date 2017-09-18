39d54216b81d9b1da6aa53d9702944fe37a796a0
Samuel Terra <samuelterra22@hotmail.com>
2017-09-18 20:16:40 -0300

    Instalação automática do sublime-text adicionada.

--- lamp.sh
+++ lamp.sh
@@ -95,7 +95,6 @@ echo "########################################################"
 
 sudo apt install -f r-base-core
 
-
 echo "########################################################"
 echo "              Instalando Sublime-text                   "
 echo "########################################################"
@@ -103,7 +102,7 @@ echo "########################################################"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get install -y sublime-text
 
 echo "########################################################"
 echo "                   Instalando Spotify                   "
